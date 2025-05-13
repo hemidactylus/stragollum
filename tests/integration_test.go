9fca1d431932050887a8c08a44b42d1eab23220c
Stefano Lottini <stefano.lottini@datastax.com>
2025-05-14 00:31:35 +0200

    integration test germ

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 package stragollum_test
 
 import (
@@ -6,4 +9,7 @@ import (
 
 func TestIntegration(t *testing.T) {
 	// TODO: Add integration test logic here
+	t.Run("DummyFailer", func(t *testing.T) {
+		t.Errorf("Boo!")
+	})
 }
